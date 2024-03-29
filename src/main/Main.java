01e1524b9fcce83729141a35adb16ea13d0502b6
Natana <naelramosfilho@gmail.com>
2024-03-29 16:02:13 -0300

    Move

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+
+package main;
+
+import View.TELA_AGENDA;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+      new TELA_AGENDA().setVisible(true);
+      
+    }
+    
+}
